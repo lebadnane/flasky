21b6e1bf60bf44cf86052f2c3166abe4c4a3b28e
adnane <the.adnane.1987@gmail.com>
2023-10-03 21:14:57 +0100

    add new react component

---
+++ frontend/src/SpeakerProfile.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const SpeakerProfile =()=> {
+    return <h1>This is a function component equivalent to a
+    class component !</h1>;
+    }
+export default SpeakerProfile;
