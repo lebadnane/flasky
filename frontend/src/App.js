37ea803f810e86da0a3a1312fda541316e58d209
adnane <the.adnane.1987@gmail.com>
2023-10-27 20:23:32 +0100

    add new react component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,40 @@
 import React from 'react';
+import SearchSpeaker from './SearchSpeaker';
 
-const SpeakerProfile=(props)=>{
-  return(
-    <>
-    <h3>{props.name}</h3>
-    <p>Position: {props.jobTitle}</p>
-    <p>Company: {props.company}</p>
-    </>
-    );
-}
-
-
-function App() {
-  const speakerName='adnaned'
+const SpeakerProfile = (props) => {
   return (
     <>
-    <h1>Speaker Profile</h1>
-    // Child component with attributes name,jobTitle and company inside parent component
-    <SpeakerProfile
-    name='Juliet Runolf'
-    jobTitle='Director,Marketing' 
-    company='Abernathy Group'/>
+      <h3>{props.name}</h3>
+      <p>Position: {props.jobTitle}</p>
+      <p>Company: {props.company}</p>
     </>
   );
-  }
-  export default App;
+}
+/*
+function App() {
+  //Child component with attributes name,jobTitle and company inside parent component
+  const speakerName = 'adnaned'
+  return (
+    <>
+      <h1>Speaker Profile</h1>
+      <SpeakerProfile
+        name='Juliet Runolf'
+        jobTitle='Director,Marketing'
+        company='Abernathy Group' />
+    </>
+  );
+}
+
+*/
+
+function App()
+{
+return (
+<div style={{backgroundColor: '#a1c4fd', margin:20,
+color:'white'}}>
+<h1></h1>
+<SearchSpeaker />
+</div>
+);
+}
+export default App;
